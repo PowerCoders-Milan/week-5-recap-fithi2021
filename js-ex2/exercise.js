d0c2c3eadcd73b4468f9b04aeebfbd177fda2d44
fithawi ghide <lgfetah@gmail.com>
2021-03-05 17:27:45 +0100

    Ex-2 Js done

---
+++ js-ex2/exercise.js
@@ -0,0 +1,34 @@
+Array1 = ['marco', 'antonio', 'luca', 13, 18, 'leo', 'mary']
+Array2 = [21, 19, 12, 'sebastian', 'will', 23, 20]
+
+var arrayOfBoth = [ ['marco',21],['antonio',19],
+                ['luca',12], ['sebastian',13],
+                ['leo',23], ['mary',20]];
+
+
+
+
+
+
+                
+
+var resultInOrder = Object.fromEntries(arrayOfBoth);
+console.log(resultInOrder);
+//output
+/*marco:21,
+  antonio:19,
+  luca:12,
+  sebastian:13,
+   will:18,
+   leo:23,
+   mary:20,
+*/
+
+ var obj = {};
+
+ for(var i = 0; i < Array1.length; i++){
+ 
+
+ }
+
+
