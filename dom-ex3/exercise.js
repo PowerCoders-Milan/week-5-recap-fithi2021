e6aee29e3c23e980bafa697695bce43b5b32bdfd
fithawi ghide <lgfetah@gmail.com>
2021-03-05 17:26:20 +0100

    Ex-3 Dom done

---
+++ dom-ex3/exercise.js
@@ -0,0 +1,16 @@
+
+var btn = document.querySelector("button");
+var p = document.getElementsByTagName("p")[0];
+    function changeEffect(){
+     p.style.backgroundColor = "red";
+    }
+    function mouseHover(){
+    
+       }
+
+    function textColor(){
+        p.style.color = "white"
+    }
+   
+
+
